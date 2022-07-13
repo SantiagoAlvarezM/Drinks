ff2e43deab4528c47b0689e42f15a59e98452a3d
Julian Alvarez <julian.alvarez@rappi.com>
2022-07-13 16:30:40 -0500

    Improve views, added Picasso rounded transformation to imageView, fix item padding

--- app/src/main/java/com/example/myapplication/util/ViewExt.kt
+++ app/src/main/java/com/example/myapplication/util/ViewExt.kt
@@ -4,5 +4,5 @@ import android.widget.ImageView
 import com.squareup.picasso.Picasso
 
 fun ImageView.loadImage(imageUrl: String) {
-    Picasso.get().load(imageUrl).into(this)
+    Picasso.get().load(imageUrl).transform(RoundedTransformation(32F, 0F)).into(this)
 }
